5c88273d2b846d21f805d4bb05077ffd7f4ce885
jang <wkdtjsgur100@naver.com>
2019-02-15 18:18:22 +0900

    Optimized 190215 medium

--- medium/1902/190215/jang.py
+++ medium/1902/190215/jang.py
@@ -3,11 +3,11 @@ for _ in range(int(input())):
     l = list(map(int, input().split()))
 
     benefit = 0
-    pivot = l[n - 1]
-    for i in range(0, n - 1)[::-1]:
-        if l[i] >= pivot:
-            pivot = l[i]
+    pivot = l[-1]
+    for i in l[::-1]:
+        if i >= pivot:
+            pivot = i
         else:
-            benefit += (pivot - l[i])
+            benefit += (pivot - i)
 
     print(benefit)
