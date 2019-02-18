c35e993356c09f5a03df0be77234a208b21735d7
cjh5414 <cjh5414@gmail.com>
2019-02-18 17:25:49 +0900

    Solve 190218 medium

---
+++ medium/1902/190218/jihun.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the organizingContainers function below.
+    static String organizingContainers(int[][] container) {
+        int n = container.length;
+        int[] sumR = new int[n];
+        int[] sumC = new int[n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < container.length; j++) {
+                sumR[i] += container[i][j];
+                sumC[i] += container[j][i];
+            }
+        }
+
+        Arrays.sort(sumR);
+        Arrays.sort(sumC);
+
+        for (int i = 0; i < n; i++)
+            if (sumR[i] != sumC[i]) return "Impossible";
+
+        return "Possible";
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int[][] container = new int[n][n];
+
+            for (int i = 0; i < n; i++) {
+                String[] containerRowItems = scanner.nextLine().split(" ");
+                scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+                for (int j = 0; j < n; j++) {
+                    int containerItem = Integer.parseInt(containerRowItems[j]);
+                    container[i][j] = containerItem;
+                }
+            }
+
+            String result = organizingContainers(container);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
