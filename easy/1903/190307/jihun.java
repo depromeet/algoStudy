7a57eac0dd59b796bbe3de3cef597caa8bc5ee51
cjh5414 <cjh5414@gmail.com>
2019-03-10 16:01:57 +0900

    Solve 190307 easy

---
+++ easy/1903/190307/jihun.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the gridChallenge function below.
+    static String gridChallenge(String[] grid) {
+        char[][] sorted = new char[grid.length][grid[0].length()];
+
+        for (int i = 0; i < grid.length; i++) {
+            sorted[i] = grid[i].toCharArray();
+            Arrays.sort(sorted[i]);
+        }
+
+        for (int c = 0; c < grid[0].length(); c++) {
+            for (int r = 0; r < grid.length - 1; r++) {
+                if (sorted[r][c] > sorted[r + 1][c]) return "NO";
+            }
+        }
+
+        return "YES";
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            String[] grid = new String[n];
+
+            for (int i = 0; i < n; i++) {
+                String gridItem = scanner.nextLine();
+                grid[i] = gridItem;
+            }
+
+            String result = gridChallenge(grid);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
