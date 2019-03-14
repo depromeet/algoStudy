5735ea2eb0379b61a3fec70c9e4911121aa0e26c
cjh5414 <cjh5414@gmail.com>
2019-03-14 19:45:26 +0900

    Solve 190306 easy

---
+++ easy/1903/190306/jihun.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the beautifulBinaryString function below.
+    static int beautifulBinaryString(String b) {
+        int count = 0;
+        StringBuilder sb = new StringBuilder(b);
+        for (int i = 0; i < b.length() - 2; i++) {
+            if (sb.substring(i, i + 3).equals("010")) {
+                sb.setCharAt(i + 2, '1');
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        String b = scanner.nextLine();
+
+        int result = beautifulBinaryString(b);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
